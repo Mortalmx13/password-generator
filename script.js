f4d824bcda63032ecd6191218843ec3bc3161f3b
Charles Young <ctyoungiv@gmail.com>
2022-05-24 20:40:36 -0400

    changed up if else statments

--- script.js
+++ script.js
@@ -23,34 +23,46 @@ alert("Your password will be " + input + " characters long")
 var lowercaseChoice = prompt("Would you like lowercase letters? yes or no")
 if(lowercaseChoice === "yes"){
   lowercaseChoice = true;
-  }else{
+  }else if(lowercaseChoice === "no"){
     lowercaseChoice = false;
+  }else{
+    alert("Please choose yes or no")
+    return generatePassword()
   }
 //uppercase choice
 var uppercaseChoice = prompt("Would you like uppercase letters? yes or no")
 if(uppercaseChoice === "yes"){
-uppercaseChoice = true;
-}else{
+  uppercaseChoice = true;
+}else if(uppercaseChoice === "no"){
   uppercaseChoice = false;
+}else{
+  alert("Please choose yes or no")
+  return generatePassword()
 }
 
-
+//number choice
 var numberChoice = prompt("Would you like numbers? yes or no")
 if(numberChoice === "yes"){
   numberChoice = true;
-  }else{
+  }else if(numberChoice === "no"){
     numberChoice = false;
+  }else{
+    alert("Please choose yes or no")
+    return generatePassword()
   }
-
+// special character choice
   var specialChoice = prompt("Would you like special characters? yes or no")
 if(specialChoice === "yes"){
   specialChoice = true;
-  }else{
-    specialChoice = false;
-  }
+}else if(specialChoice === "no"){
+  specialChoice = false;
+}else{
+  alert("Please choose yes or no")
+  return generatePassword()
+}
 
 if(lowercaseChoice === false && uppercaseChoice === false && numberChoice === false && specialChoice === false){
-  alert("A character needs to be chosen");
+  alert("Please choose a character type");
   return generatePassword();
 
 }
