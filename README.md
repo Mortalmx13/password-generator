cd2c18e8d93a6a92836755c5922349e55fe0f197
Charles Young <ctyoungiv@gmail.com>
2022-05-24 23:09:58 -0400

    edited the readme

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # password-generator
+
+
+
+# Table of Contents
+
+1. [Description](#description)
+2. [Visuals](#visuals)
+3. [Resources](#resources)
+
+## Description
+
+this assignment was to make a randomly generated password in java script. this was to have a better understanding on how to set up functions and alerts. 
+
+## Visuals
+![Portfolio](./images/Password-Generator.png)
+
+## Resources
+- [Live Site](https://mortalmx13.github.io/portfolio/)
+
+- [Respository](https://github.com/Mortalmx13/portfolio)
--- script.js
+++ script.js
@@ -60,58 +60,47 @@ if(specialChoice === "yes"){
   alert("Please choose yes or no")
   return generatePassword()
 }
-
+//if they do not agree to nay of the characters
 if(lowercaseChoice === false && uppercaseChoice === false && numberChoice === false && specialChoice === false){
   alert("Please choose a character type");
   return generatePassword();
 
 }
-
-retVal = "";
+//sets up the random leters and numbers
+val = "";
 for( count = 0; count < input; ){
   if(lowercaseChoice=== true){
     
-    retVal += lowercase.charAt(Math.floor(Math.random()* lowercase.length));
+    val += lowercase.charAt(Math.floor(Math.random()* lowercase.length));
     count++;
   }
   if(uppercaseChoice === true){
     if(count < input){
-      retVal += uppercase.charAt(Math.floor(Math.random()* uppercase.length));
+      val += uppercase.charAt(Math.floor(Math.random()* uppercase.length));
     count++;
     }
     
   }
   if(numberChoice === true){
     if(count < input){
-      retVal += numbsers.charAt(Math.floor(Math.random()* numbsers.length));
+      val += numbsers.charAt(Math.floor(Math.random()* numbsers.length));
     count++;
     }
     
   }
   if(specialChoice === true){
     if(count < input){
-      retVal += special.charAt(Math.floor(Math.random()* special.length));
+      val += special.charAt(Math.floor(Math.random()* special.length));
       count++;
     }
    
   }
 
-  
-
-
-
-
+}
+//sends the pasword into the box
+return val
 
 }
-console.log(retVal)
-return retVal
-
-//console.log(uppercaseChoice);
-//console.log(lowercaseChoice)
-//return writePassword
-}
-
-
 
 // Write password to the #password input
 function writePassword() {
@@ -127,19 +116,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-// GIVEN I need a new, secure password
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-// WHEN asked for character types to include in the password
-// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
